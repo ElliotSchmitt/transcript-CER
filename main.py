c42a1c28dc12c12973e1402a878d59a8b3f94da9
JacobSchmitt <jacobschmitt00@gmail.com>
2022-07-15 09:40:19 -1000

    Calculate Character Error Rate of podcast transcriptions

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from evaluate import load
 
+#ref is the reference file we compare the transcription to
 cer = load("cer")
 ref = open('/Users/intern/PycharmProjects/pythonProject/F512_ref_file.txt', 'r')
 pred = open('/Users/intern/PycharmProjects/pythonProject/F512F509-7916-49C0-BFF7-73C702BF2A66_transcript.txt', 'r')
+#put input into string
 referenceString = ''
 predictionString = ''
 for i in ref.read():
@@ -11,6 +13,7 @@ for i in ref.read():
 for i in pred.read():
     if i != '' and i != '\n':
         predictionString += i
+#CER wants lists as input
 references = [referenceString]
 predictions = [predictionString]
 cer_score = cer.compute(predictions=predictions, references=references)
